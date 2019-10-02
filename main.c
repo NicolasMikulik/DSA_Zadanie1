37399367a81125f02293c960f5f31d877e22e128
Nicolas Mikulik <nicolas.mikulik01@gmail.com>
2019-10-02 07:02:17 +0200

    Committing to save draft before applying changes

--- main.c
+++ main.c
@@ -1,62 +1,163 @@
 #include<stdio.h>
 #include <string.h>
-#define INTCOUNT 100
-int *memory;
+#define BYTECOUNT 100
+char *memory;
+
+typedef struct block{
+    int size;
+    struct block *next;
+};
+
 void memory_init(void *ptr, unsigned int size){
-    memset(ptr,0,size);
     memory=(void*)ptr;
-    *memory = INTCOUNT;   //size of array in integers == INTCOUNT 100
-    *(memory+1)=2;  //"pointer" to the first free block == 8
+    *memory = BYTECOUNT;   //size of array in integers == BYTECOUNT 100
+    *(memory+1)='h';  //"pointer" to the first free block == 8
     *(memory+2)=(size/sizeof(int)) - 3*sizeof(int); //size of first free block in integers == 88
     *(memory+3)=0;  //"pointer" to next free block. At the start it is zero, because there is only one free block == 0
 }
+
 void *memory_alloc(unsigned int size){
     int *curr = (void*)memory;//+*(memory+1)/sizeof(int);
     int *prev, *result;
     curr++;
-    printf("curr %d memory+INTCOUNT %d\n",*curr,*(memory+99));
-    while(curr<(memory+INTCOUNT-1) && ((*curr)<=0 || (*curr)<size/sizeof(int))){
-        prev=curr;
-        curr += (*curr);
-        printf("One block checked\n");
-    }
+
     printf("Curr found a suitable block of size: %d\n",*curr);
 }
+
 int main(){
 
-    /*int *p=(int)MyMalloc(100*sizeof(int));
-    char *q=(char)MyMalloc(250*sizeof(char));
-    int *r=(int)MyMalloc(1000*sizeof(int));
-    MyFree(p);
-    char *w=(char)MyMalloc(700);
-    MyFree(r);
-    int *k=(int)MyMalloc(500*sizeof(int));
-    printf("Allocation and deallocation is done successfully!");*/
-
-    /*
-     int main()
-    {
-    char region[50];
-    memory_init(region, 50);
-    char* pointer = (char*) memory_alloc(10);
-    if (pointer)
-    memset(pointer, 0, 10); //na adresu, kam ukazuje pointer, sa zapise velkost bloku
-    if (pointer)
-    memory_free(pointer);
-    return 0;
-    }
-    */
-
-    int region[INTCOUNT];
-    memory_init(region,INTCOUNT*sizeof(int));
-    printf("%d %d %d %d %d\n",region[0],region[1],region[2],region[3], region[INTCOUNT-1]);
-    region[INTCOUNT-1] = -1;    //end of array
+    char region[BYTECOUNT];
+    memory_init(region,BYTECOUNT*sizeof(char));
+    int testchar = 254;
+    printf("%d %c %c %c %c %c\n",region[0],region[1],region[2],region[3], region[BYTECOUNT-1],testchar);
+    testchar -= 154;
+    printf("%c %d\n",testchar,(int)testchar);
+    testchar += 27;
+    printf("%c %d %lujd\n",testchar,(int)testchar,sizeof(char));
+    region[BYTECOUNT-1] = -1;    //end of array
     int *pointer1=(int*)memory_alloc(12);    //allocating space for two integers == 8 Bytes
+    printf("%ld\n", sizeof(struct block));
     return 0;
 }
 
-/*int array[5] = {1,2,3,4,5};
-    int *pointer = &array[0];
-    printf("pointer points at the first element of array == %d\n", *pointer);
-    pointer += sizeof(int)/4;
-    printf("pointer points at the second element of array == %d\n", *pointer);*/
+/*
+
+malloc.c
+void initialize(){
+ freeList->size=20000-sizeof(struct block);
+ freeList->free=1;
+ freeList->next=NULL;
+}
+
+void split(struct block *fitting_slot,size_t size){
+ struct block *new=(void*)((void*)fitting_slot+size+sizeof(struct block));
+ new->size=(fitting_slot->size)-size-sizeof(struct block);
+ new->free=1;
+ new->next=fitting_slot->next;
+ fitting_slot->size=size;
+ fitting_slot->free=0;
+ fitting_slot->next=new;
+}
+
+
+void *MyMalloc(size_t noOfBytes){
+ struct block *curr,*prev;
+ void *result;
+ if(!(freeList->size)){
+  initialize();
+  printf("Memory initialized\n");
+ }
+ curr=freeList;
+ while((((curr->size)<noOfBytes)||((curr->free)==0))&&(curr->next!=NULL)){
+  prev=curr;
+  curr=curr->next;
+  printf("One block checked\n");
+ }
+ if((curr->size)==noOfBytes){
+  curr->free=0;
+  result=(void*)(++curr);
+  printf("Exact fitting block allocated\n");
+  return result;
+ }
+ else if((curr->size)>(noOfBytes+sizeof(struct block))){
+  split(curr,noOfBytes);
+  result=(void*)(++curr);
+  printf("Fitting block allocated with a split\n");
+  return result;
+ }
+ else{
+  result=NULL;
+  printf("Sorry. No sufficient memory to allocate\n");
+  return result;
+ }
+}
+
+void merge(){
+ struct block *curr,*prev;
+ curr=freeList;
+ while((curr->next)!=NULL){
+  if((curr->free) && (curr->next->free)){
+   curr->size+=(curr->next->size)+sizeof(struct block);
+   curr->next=curr->next->next;
+  }
+  prev=curr;
+  curr=curr->next;
+ }
+}
+
+void MyFree(void* ptr){
+ if(((void*)memory<=ptr)&&(ptr<=(void*)(memory+20000))){
+  struct block* curr=ptr;
+  --curr;
+  curr->free=1;
+  merge();
+ }
+ else printf("Please provide a valid pointer allocated by MyMalloc\n");
+}
+
+
+
+malloc.h
+
+
+#include<stdio.h>
+#include<stddef.h>
+
+char memory[20000];
+
+struct block{
+ size_t size;
+ int free;
+ struct block *next;
+};
+
+struct block *freeList=(void*)memory;
+
+void initialize();
+void split(struct block *fitting_slot,size_t size);
+void *MyMalloc(size_t noOfBytes);
+void merge();
+void MyFree(void* ptr);
+
+
+
+Main.c
+
+
+#include<stdio.h>
+
+int main(){
+
+ int *p=(int)MyMalloc(100*sizeof(int));
+ char *q=(char)MyMalloc(250*sizeof(char));
+ int *r=(int)MyMalloc(1000*sizeof(int));
+ MyFree(p);
+ char *w=(char)MyMalloc(700);
+ MyFree(r);
+ int *k=(int)MyMalloc(500*sizeof(int));
+ printf("Allocation and deallocation is done successfully!");
+
+}
+
+
+ */
